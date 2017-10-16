929e9947eebee2cb77930f702986f0de85183ac3
Anx <anxsec@gmail.com>
2017-10-16 17:45:57 +0800

    add data structure

---
+++ data_structure/hash_table.js
@@ -0,0 +1,30 @@
+function chained_hash_insert(table, x) {
+  
+}
+
+function chained_hash_search(table, key) {
+  
+}
+
+function chained_hash_delete(table, x) {
+
+}
+
+function division_hash() {
+  
+}
+
+function multiplication_hash() {
+
+}
+
+function universal_hash() {
+  
+}
+
+// test chained hash table
+
+const hash_table = {};
+
+chained_hash_insert(hash_table, {key: 1, value: 1});
+chained_hash_insert(hash_table, {key: 11, value: 11});
